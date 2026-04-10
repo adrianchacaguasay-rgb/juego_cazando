9e90716000449c4a1dc28692ef8f0f34f872f3ed
adrianchacaguasay-rgb <adrianchacaguasay@gmail.com>
2026-04-09 19:44:53 -0500

    Definir gato y comida, dibujo movimiento

---
+++ cazando.js
@@ -0,0 +1,66 @@
+let canvas=document.getElementById("juego");
+let ctx=canvas.getContext("2d");
+let btnArriba= document.getElementById("btnArriba");
+let btnAbajo= document.getElementById("btnAbajo");
+let btnIzquierda= document.getElementById("btnIzquierda");
+let btnDerecha= document.getElementById("btnDerecha");
+const VELOCIDAD=15;
+
+
+//Gato
+let gatoX=0;
+let gatoY=0;
+const ANCHOGATO= 50;
+const ALTURAGATO=50; 
+ 
+//Raton
+let comidaX=0;
+let comidaY=0;
+const ANCHOCOMIDA=30;
+const ALTUCOMIDA=30;
+
+function graficar(x,y,ancho,alto,color){
+    ctx.fillStyle=color;
+    ctx.fillRect(x,y,ancho,alto);
+}
+
+function graficarGato(){    
+    graficar(gatoX,gatoY,ANCHOGATO,ALTURAGATO,"#0D4D82");
+
+}   
+
+function graficarcomida(){   
+ graficar(comidaX,comidaY,ANCHOCOMIDA,ALTUCOMIDA,"#820D7A");
+
+
+}
+
+
+
+//Funcion iniciar juego 
+function iniciarJuego(){   
+    gatoX = (canvas.width / 2) - (ANCHOGATO / 2);   
+    gatoY = (canvas.height / 2) - (ALTURAGATO / 2); 
+
+    comidaX = (canvas.width / 2) - (ANCHOCOMIDA / 2);   
+    comidaY = (canvas.height / 2) - (ALTUCOMIDA / 2); 
+    graficarGato();
+    graficarcomida();
+ 
+}               
+
+function mover(direccion){  
+    if (direccion === "arriba") gatoY -= VELOCIDAD;         
+    if (direccion === "abajo") gatoY += VELOCIDAD;      
+    if (direccion === "izquierda") gatoX -= VELOCIDAD;      
+    if (direccion === "derecha") gatoX += VELOCIDAD;
+    graficarGato();
+ 
+
+}
+document.getElementById("btnArriba").onclick = () => mover("arriba");       
+document.getElementById("btnAbajo").onclick = () => mover("abajo"); 
+document.getElementById("btnIzquierda").onclick = () => mover("izquierda"); 
+document.getElementById("btnDerecha").onclick = () => mover("derecha");
+
+iniciarJuego(); 
